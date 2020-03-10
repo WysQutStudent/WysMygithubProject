3667dd06fd8d93a60c66aa24cf63452ecbd41d3d
WYS <1606047872@qq.com>
2020-03-10 10:23:22 +0800

    这是第二次从IDEA到git

--- src/servlets/HelloServlet.java
+++ src/servlets/HelloServlet.java
@@ -13,7 +13,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.println("Hello github project!");
+        writer.println("Hello My github project!");
 
     }
 }
